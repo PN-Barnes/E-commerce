e4c9a8fefb0472977f30dab5685deb1a04c53c80
PN-Barnes <paulieb99@gmail.com>
2021-07-28 21:57:13 -0700

    added associations within the index.js for categories and products, THEN wrote the first get all statement within category routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,15 +6,42 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const catdata = await Category.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(catdata);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  try {
+    const catId = await Category.findByPk(req.params.id);
+
+    if(!catId) {
+      res.status(404).json({ message: 'No Category with this Id!'})
+    }
+
+    
+
+  } catch (error) {
+    res.status(400).json(err);
+  }
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCat= req.body;
+
+    
+  } catch (error) {
+    
+  }
 });
 
 router.put('/:id', (req, res) => {
