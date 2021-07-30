47982875b5f8f6f3120c0f0a15b61cad89483df7
PN-Barnes <paulieb99@gmail.com>
2021-07-29 20:17:24 -0700

    created get routes for tag and tag id, tested their ability

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -2,15 +2,36 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
+// * SUCCESSFUL GET ROUTE FOR ALL
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product}]
+    })
+    res.status(200).json(tagData)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
-
-router.get('/:id', (req, res) => {
+// * SUCCESSFUL GET ROUTE FOR ID
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, 
+      {
+        include: [{model: Product}]
+      })
+      if(!tagData) {
+        res.status(404).json({message: 'No Tag with this ID!'})
+        return;
+      }
+      res.status(200).json(tagData)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 router.post('/', (req, res) => {
