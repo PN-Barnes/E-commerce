1bd50f172fa8c7325f7eff251fea678526c4e03a
PN-Barnes <paulieb99@gmail.com>
2021-07-29 22:35:44 -0700

    completed delete route within tag-routes file

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -40,10 +40,25 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
-});
+}); 
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+
+    if(!deleteTag) {
+      res.status(404).json({message: "No tag with specidfied ID!"})
+      return;
+    }
+    res.status(200).json(deleteTag)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 module.exports = router;
